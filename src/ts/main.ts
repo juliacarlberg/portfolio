5511b7eacd88cfadc8913e36b6c232f70054a20d
Julia Carlberg <juliacarlbrg@gmail.com>
2022-11-14 20:08:58 +0100

    second commit

---
+++ src/ts/main.ts
@@ -0,0 +1,9 @@
+let kontakt= document.getElementById("kontakt");
+
+/*function kontaktalert(){
+    kontakt?.addEventListener("click", function(){
+        alert("Du klickade på kontakt!");
+    })
+}
+
+kontaktalert(); */
