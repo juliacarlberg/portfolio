b020bd7431ec90c4b033dea726218489a2e85cd9
Julia Carlberg <juliacarlbrg@gmail.com>
2023-01-26 21:33:46 +0100

    small changes

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,7 @@
 let kontakt= document.getElementById("kontakt");
 
+let x: number = 0;
+
 /*function kontaktalert(){
     kontakt?.addEventListener("click", function(){
         alert("Du klickade på kontakt!");
