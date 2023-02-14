a30cca839cb0197d19b7357be00e88360ab3215b
Julia Carlberg <juliacarlbrg@gmail.com>
2023-02-14 11:57:36 +0100

    changes

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -19,3 +19,105 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+let burgerBtn: any = document.getElementById("burgerBtn");
+let burgerModal: any = document.getElementById("myBurgerModal");
+
+burgerBtn.onclick = function () {
+  burgerModal.style.display = "block";
+};
+
+window.onclick = function (event) {
+  if (event.target == burgerModal) {
+    burgerModal.style.display = "none";
+  }
+};
+
+//slide prickar
+let slideIndex = 1;
+showDivs(slideIndex);
+showMobileDivs(slideIndex);
+
+function plusDivs(n: number) {
+  showDivs((slideIndex += n));
+  showMobileDivs((slideIndex += n));
+}
+
+function currentDiv(n: number) {
+  showDivs((slideIndex = n));
+  showMobileDivs((slideIndex = n));
+}
+
+function showDivs(n: number) {
+  let i;
+  let x = document.getElementsByClassName(
+    "mySlides"
+  ) as HTMLCollectionOf<HTMLElement>;
+  let dots = document.getElementsByClassName("desktop-dot");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " w3-white";
+}
+
+function showMobileDivs(n: number) {
+  let i;
+  let x = document.getElementsByClassName(
+    "myMobileSlides"
+  ) as HTMLCollectionOf<HTMLElement>;
+  let dots = document.getElementsByClassName("w3-badge");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " w3-white";
+}
+
+//text animation
+
+let i = 0;
+
+function typeWriter() {
+  let txt = "Julia Carlberg";
+  let speed = 80;
+  if (i < txt.length) {
+    (document.getElementById("demo") as HTMLElement).innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+let index = 0;
+
+function typeWriter2() {
+  let text = "Frontend-utvecklare";
+  let speed = 80;
+  if (index < text.length) {
+    (document.getElementById("typeMe") as HTMLElement).innerHTML +=
+      text.charAt(index);
+    index++;
+    setTimeout(typeWriter2, speed);
+  }
+}
+
+typeWriter();
+typeWriter2();
